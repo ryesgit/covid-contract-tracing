272856919a3e7c4546f4812782611ad3e59cdd05
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-07-17 08:38:08 +0800

    Draw sample user data on the UI

--- CovidTracingUI.py
+++ CovidTracingUI.py
@@ -10,6 +10,9 @@ class UI:
         self.__frame.grid(row=0, column=0, sticky="N W E S")
 
         self.__display_headers("Name", "Age")
+        user_data = read_json('contacts.json')
+        self.__display_user_data(user_data)
+
         self.__master.mainloop()
 
     def __display_headers(self, *args):
@@ -17,3 +20,27 @@ class UI:
         for idx, header in enumerate(args):
             label = ttk.Label(self.__frame, padding=5); label.grid(row=0, column=idx)
             label['text'] = header
+
+    def __display_user_data(self, data: List[dict]):
+        '''
+        Draws the user data on the UI.
+
+        Parameters
+        ----------
+        data : List[dict]
+            A list of dictionaries containing user data.
+        '''
+        for idx, user in enumerate(data):
+            label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=0)
+            label['text'] = data[user]['name']
+            label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=1)
+            label['text'] = data[user]['age']
+
+def read_json(path):
+    '''
+    Reads the sample JSON file and returns a list of dictionaries.
+    '''
+    import json
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
