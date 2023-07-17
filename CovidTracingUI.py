e94f0c911746b3eac01758347f10f6d14f87381d
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-07-17 19:18:28 +0800

    Separate table component class

--- CovidTracingUI.py
+++ CovidTracingUI.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import ttk
 
 from typing import List
+from ContactsIO import ContactsIO
+from components.Table import Table
 
 class UI:
     def __init__(self) -> None:
@@ -9,8 +11,12 @@ class UI:
         self.__frame = ttk.Frame(self.__master)
         self.__frame.grid(row=0, column=0, sticky="N W E S")
 
-        self.__display_headers("Name", "Age")
-        user_data = read_json('contacts.json')
+        headers = ["Name", "Age"]
+
+        # Initialize contacts delegator
+        contacts_delegator = ContactsIO('contacts.json')
+
+        user_data = contacts_delegator.get_user_data()
         self.__display_user_data(user_data)
 
         add_contact_button = ttk.Button(self.__frame, text="Add Contact", command=self.show_form_window)
@@ -21,12 +27,6 @@ class UI:
 
         self.__master.mainloop()
 
-    def __display_headers(self, *args):
-        
-        for idx, header in enumerate(args):
-            label = ttk.Label(self.__frame, padding=5); label.grid(row=0, column=idx)
-            label['text'] = header
-
     def __display_user_data(self, data: List[dict]):
         '''
         Draws the user data on the UI.
@@ -36,11 +36,13 @@ class UI:
         data : List[dict]
             A list of dictionaries containing user data.
         '''
-        for idx, user in enumerate(data):
-            label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=0)
-            label['text'] = data[user]['name']
-            label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=1)
-            label['text'] = data[user]['age']
+        table = Table(self.__frame, data)
+
+        # for idx, user in enumerate(data):
+        #     label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=0)
+        #     label['text'] = data[user]['name']
+        #     label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=1)
+        #     label['text'] = data[user]['age']
 
     def show_form_window(self):
         '''
@@ -54,16 +56,6 @@ class UI:
 
         # Center this window
         center_window(self.__form_window)
-
-
-def read_json(path):
-    '''
-    Reads the sample JSON file and returns a list of dictionaries.
-    '''
-    import json
-    with open(path, 'r') as f:
-        data = json.load(f)
-    return data
 
 def center_window(window: Tk | Toplevel):
         '''
---
+++ components/Table.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import ttk
+from typing import List, Callable
+
+class Table:
+    '''
+    Utilizes the TK library to create a table of data.
+    '''
+    def __init__(self, frame: ttk.Frame, data: List[dict]):
+        '''
+        Parameters
+        ----------
+        frame : ttk.Frame
+            The frame to place the table on.
+        headers : List[str]
+            The headers of the table.
+        data : List[dict]
+            The data to display on the table.
+        '''
+        self.__table_frame = ttk.Frame(frame); self.__table_frame.grid(row=0, column=0, sticky="N W E S")
+        self.__headers = [attribute for attribute in data[list(data.keys())[0]]]
+        self.__draw_headers()
+        self.__data = data
+        self.__draw_user_data()
+
+    def __draw_headers(self):
+        '''
+        Draws the headers of the table.
+        '''
+        for idx, header in enumerate(self.__headers):
+            label = ttk.Label(self.__table_frame, padding=5); label.grid(row=0, column=idx)
+            label['text'] = header
+
+    def __draw_user_data(self):
+        for row, user in enumerate(self.__data):
+            for column in range(len(self.__headers)):
+                label = ttk.Label(self.__table_frame, padding=5); label.grid(row=row+1, column=column)
+                label["text"] = self.__data[user][self.__headers[column]]
