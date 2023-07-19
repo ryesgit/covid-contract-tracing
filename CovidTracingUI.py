15e5a6a81ebcc7ba70bf0742b150887e41cf679a
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-07-19 12:55:06 +0800

    Add scrollbar to view overflowing contacts

--- CovidTracingUI.py
+++ CovidTracingUI.py
@@ -13,18 +13,18 @@ class UI:
         self.__master.columnconfigure(0, weight=1)
         self.__master.rowconfigure(0, weight=1)
 
-        self.__master.minsize(250, 250)
-
         self.__frame = ttk.Frame(self.__master)
         self.__frame.grid(row=0, column=0, sticky="N W E S")
+        self.__frame.grid_columnconfigure(0, weight=1)
+        self.__frame.grid_rowconfigure(0, weight=1)
 
-        headers = ["Name", "Age"]
+        self.__frame.configure(borderwidth=5, relief="sunken")
 
         self.__display_user_data()
 
         add_contact_button = ttk.Button(self.__frame, text="Add Contact", command=self.show_form_window)
         add_contact_button.grid(row=self.__frame.grid_size()[1], column=0, columnspan=2, sticky="W E")
-        
+
         # Center the window
         center_window(self.__master)
 
@@ -47,13 +47,26 @@ class UI:
 
         contacts_delegator = ContactsIO('contacts.json')
         data = contacts_delegator.get_user_data()
-        table = Table(self.__frame, data)
 
-        # for idx, user in enumerate(data):
-        #     label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=0)
-        #     label['text'] = data[user]['name']
-        #     label = ttk.Label(self.__frame, padding=5); label.grid(row=idx+1, column=1)
-        #     label['text'] = data[user]['age']
+
+        
+        canvas = Canvas(self.__frame)
+        canvas.grid(row=0, column=0, sticky="N W E S")
+
+        scrollbar = Scrollbar(self.__frame, orient="vertical", command=canvas.yview)
+        scrollbar.grid(row=0, column=1, sticky="N S E")
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        table_frame = ttk.Frame(canvas)
+        table_frame.configure(borderwidth=5, relief="sunken")
+        canvas.create_window((0, 0), window=table_frame, anchor="nw", tags="table_frame")
+        Table(table_frame, data)
+        
+        canvas.bind("<Configure>", lambda event: canvas.configure(scrollregion=canvas.bbox("all")))
+        
+
+
+        # Resize the canvas to fill 5 items from the frame
 
     def show_form_window(self):
         '''
