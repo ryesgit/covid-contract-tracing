41e539fa2ab42e930a02f797ee4adb6e9bca9ac7
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-07-23 21:05:54 +0800

    Draw search contact form

--- components/SearchContactsForm.py
+++ components/SearchContactsForm.py
@@ -1,6 +1,7 @@
 from typing import List
 from tkinter import *
 from tkinter import ttk
+from functools import partial
 
 class SearchContactsForm:
     '''
@@ -15,8 +16,8 @@ class SearchContactsForm:
         and retunts a list of contacts
         that match the category
 
-    rerender_canvas(contacts: List[dict]) -> None
-        Rerenders the canvas with the newly searched contacts
+    render_canvas(contacts: List[dict]) -> None
+        Renders the canvas with the searched contacts
     '''
     def __init__(self, master, contacts: List[dict]) -> None:
         '''
@@ -36,6 +37,29 @@ class SearchContactsForm:
 
         title = ttk.Label(self.__form_window, text="Search Contacts Form", padding=5)
         title.grid(row=0, column=0, columnspan=2)
+
+        self.__draw_form()
+
+    def __draw_form(self):
+        '''
+        Draws the search contact form
+        '''
+        from components.EntryTable import EntryTable
+
+        if self.__contacts:
+            # Set first contact's categories as default categories
+            categories = []
+            print(self.__contacts)
+            for key in self.__contacts[0]:
+                categories.append(key)
+
+            self.__headers = categories
+
+            # Create combobox for each category
+            combobox = partial(ttk.Combobox, values=categories, state="readonly")
+
+
+            EntryTable(self.__form_window, ["Search by Category", "Value"], [combobox, ttk.Entry], on_submit=lambda e: self.render_canvas(self.search_by_category(e[0], e[1])))
 
     def get_window(self):
         return self.__form_window
